1a5947a46b4a51c747619f20c3e709edfd7195c2
DESKTOP-7MM2106\unity <aleksander.conushin>
2022-10-02 22:33:38 +0700

    Edit scripts

--- Assets/1_Scripts/Character/PlayerController.cs
+++ Assets/1_Scripts/Character/PlayerController.cs
@@ -3,23 +3,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IPunObservable
 {
+    [SerializeField] private Sprite enemySprite;
+    [SerializeField] private Transform ladder;
+
     private PhotonView photonView;
     private SpriteRenderer spriteRenderer;
 
     private Vector2Int direction;
-    private Vector2Int gamePosition;
 
     private float speedMove = 10;
 
-    public Vector2Int GetGamePosition { get => gamePosition; }
+    public Vector2Int GamePosition;
+    public PhotonView GetPhotonView { get => photonView; }
+    public Vector2Int Direction { get => direction; set => direction = value; }
 
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        gamePosition = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+        GamePosition = new Vector2Int((int)transform.position.x, (int)transform.position.y);
         MapController.Instance.AddPlayer(this);
+
+        if (!photonView.IsMine) spriteRenderer.sprite = enemySprite;
     }
 
     private void Update()
@@ -32,10 +38,24 @@ public class PlayerController : MonoBehaviour, IPunObservable
             if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) direction = Vector2Int.down;
         }
 
-        if (direction == Vector2Int.left) spriteRenderer.flipX = false;
-        if (direction == Vector2Int.right) spriteRenderer.flipX = true;
+        //if (direction == Vector2Int.left) spriteRenderer.flipX = false;
+        //if (direction == Vector2Int.right) spriteRenderer.flipX = true;
 
-        transform.position = Vector3.MoveTowards(transform.position, (Vector2)gamePosition, Time.deltaTime * speedMove);
+        transform.position = Vector3.MoveTowards(transform.position, (Vector2)GamePosition, Time.deltaTime * speedMove);
+    }
+
+    public void SetLadderLength(int length)
+    {
+        for (int i = 0; i < ladder.childCount; i++)
+        {
+            ladder.GetChild(i).gameObject.SetActive(i < length);
+        }
+
+        while (ladder.childCount < length)
+        {
+            Transform lastTile = ladder.GetChild(ladder.childCount - 1);
+            Instantiate(lastTile, lastTile.position + Vector3.down, Quaternion.identity, ladder);
+        }
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
--- Assets/1_Scripts/MapController.cs
+++ Assets/1_Scripts/MapController.cs
@@ -1,7 +1,11 @@
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class MapController : Singleton<MapController>
+public class MapController : Singleton<MapController>, IOnEventCallback
 {
     [SerializeField] private GameObject cellPrefab;
     [SerializeField] private int width, height;
@@ -9,11 +13,13 @@ public class MapController : Singleton<MapController>
     private GameObject[,] cells;
     private List<PlayerController> players = new List<PlayerController>();
 
+    private double lastTickTime;
+
     public void AddPlayer(PlayerController player)
     {
         players.Add(player);
 
-        cells[player.GetGamePosition.x, player.GetGamePosition.y].SetActive(false);
+        cells[player.GamePosition.x, player.GamePosition.y].SetActive(false);
     }
 
     private void Start()
@@ -27,5 +33,119 @@ public class MapController : Singleton<MapController>
                 cells[w, h] = Instantiate(cellPrefab, new Vector3(w, h), Quaternion.identity, transform);
             }
         }
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.Time > lastTickTime + 1 && PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            Vector2Int[] directions = players
+                .OrderBy(p => p.GetPhotonView.Owner.ActorNumber)
+                .Select(p => p.Direction)
+                .ToArray();
+
+            RaiseEventOptions options = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+            SendOptions sendOptions = new SendOptions { Reliability = true };
+            PhotonNetwork.RaiseEvent(42, directions, options, sendOptions);
+
+            PerformTick(directions);
+        }
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        switch (photonEvent.Code)
+        {
+            case 42:
+                Vector2Int[] directions = (Vector2Int[])photonEvent.CustomData;
+
+                PerformTick(directions);
+
+                break;
+        }
+    }
+
+    private void PerformTick(Vector2Int[] directions)
+    {
+        if (players.Count != directions.Length) return;
+
+        PlayerController[] sortedPlayers = players
+            .OrderBy(p => p.GetPhotonView.Owner.ActorNumber)
+            .ToArray();
+
+        int i = 0;
+
+        foreach (var player in sortedPlayers)
+        {
+            player.Direction = directions[i++];
+
+            MinePlayerBlock(player);
+        }
+
+        foreach (var player in sortedPlayers)
+        {
+            MovePlayer(player);
+        }
+
+        lastTickTime = PhotonNetwork.Time;
+    }
+
+    private void MinePlayerBlock(PlayerController player)
+    {
+        Vector2Int targetPosition = player.GamePosition + player.Direction;
+
+        if (targetPosition.x < 0) return;
+        if (targetPosition.y < 0) return;
+        if (targetPosition.x >= width - 1) return;
+        if (targetPosition.y >= height - 1) return;
+
+        cells[targetPosition.x, targetPosition.y].SetActive(false);
+
+        Vector2Int pos = targetPosition;
+        PlayerController minePlayer = players.First(p => p.GetPhotonView.IsMine);
+
+        if (minePlayer != player)
+        {
+            while (pos.y < cells.GetLength(1) && !cells[pos.x, pos.y].activeSelf)
+            {
+                if(pos == minePlayer.GamePosition)
+                {
+                    PhotonNetwork.LeaveRoom();
+                    break;
+                }
+
+                pos.y++;
+            }
+        }
+    }
+
+    private void MovePlayer(PlayerController player)
+    {
+        player.GamePosition += player.Direction;
+
+        if (player.GamePosition.x < 0) player.GamePosition.x = 0;
+        if (player.GamePosition.y < 0) player.GamePosition.y = 0;
+        if (player.GamePosition.x >= width - 1) player.GamePosition.x = width - 1;
+        if (player.GamePosition.y >= height - 1) player.GamePosition.y = height - 1;
+
+        int ladderLength = 0;
+        Vector2Int pos = player.GamePosition;
+        while (pos.y > 0 && !cells[pos.x, pos.y - 1].activeSelf)
+        {
+            ladderLength++;
+            pos.y--;
+        }
+
+        player.SetLadderLength(ladderLength);
+    }
+
+    public void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
     }
 }
